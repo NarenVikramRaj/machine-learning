68bf31ba0944a19bb26890c5aa7c2957bf428958
NarenVikramRaj <nmasna@ufl.edu>
2019-01-27 21:17:53 -0500

    cost function added

---
+++ temp.m
@@ -0,0 +1,15 @@
+x=1:100;
+y=1000:100:10900;
+m=length(x);
+teta_0=0;
+teta_1=-100:100;
+sum=0;
+j=zeros(length(teta_1),1);
+for i=1:length(teta_1)
+    for k=1:m
+        temp=((teta_1(i)*x(k))-y(k))^2;
+        sum=sum+temp;
+    end
+    j(i)=sum/(2*m);
+end
+plot(teta_1,j)
